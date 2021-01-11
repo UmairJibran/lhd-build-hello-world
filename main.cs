7701ed8615aa4203c8dc6f3940d11b59c9f1fbad
Umair Jibran <umairjibran7@gmail.com>
2021-01-11 15:05:24 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("It's my first Local Hack Day!");
+    var name = "Umair Jibran";
+    Console.WriteLine ($"My name is {name}");
+  }
+}
